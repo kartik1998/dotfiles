b8280dd9141409b331f7a70880304c777742c110
Kartik <kartikrawat61@gmail.com>
2020-11-28 20:18:19 +0530

    Add seperate script for vim setup

---
+++ vim_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function check_nvim {
+while true; do
+    read -p "Please Install nvim before setup starts. Is nvim installed? [Y/n]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please install nvim first." 
+		     exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done 
+}
+
+check_nvim
+
+rm -rf .vim*
+
+# Setup neovim 
+mkdir -p $HOME/.config
+cp -r ./vim $HOME/.config/
+mv $HOME/.config/vim $HOME/.config/nvim
+
+# Install neovim plugins
+nvim +PlugInstall
+
+
+
