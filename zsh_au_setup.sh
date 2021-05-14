22e12816b1733a43b4276a979e1fe6612d23024c
Kartik <kartik@hyperverge.co>
2021-05-14 17:03:25 +0530

    Patch: sed to update to powerlevel10k

--- zsh_au_setup.sh
+++ zsh_au_setup.sh
@@ -13,19 +13,5 @@ echo "source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 
 sudo git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
-function escape_slashes {
-    sed 's/\//\\\//g' 
-}
-
-function change_line {
-    local OLD_LINE_PATTERN=$1; shift
-    local NEW_LINE=$1; shift
-    local FILE=$1
-
-    local NEW=$(echo "${NEW_LINE}" | escape_slashes)
-    sed -i .bak '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
-    mv "${FILE}.bak" /tmp/
-}
-
-change_line 'ZSH_THEME="robbyrussell"' 'ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc 
+sed -i '/ZSH_THEME="robbyrussell"/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
 
