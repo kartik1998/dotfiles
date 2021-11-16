7889309bd1f3ac232748c697dff7c8ecff486927
Kartik <kartikrawat61@gmail.com>
2021-11-17 00:38:42 +0530

    Update and rename vim_setupp.sh to vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,10 +1,9 @@
-# get vimrc 
-curl -o $HOME/.vimrc https://raw.githubusercontent.com/kartik1998/dotfiles/master/vim/.vimrc
-
 # get colorscheme
-rm -rf $HOME/.vim
 mkdir -p $HOME/.vim/colors
 curl -o $HOME/.vim/colors/hybrid.vim https://raw.githubusercontent.com/kartik1998/dotfiles/master/vim/colors/hybrid.vim
+
+# get vimrc 
+curl -o $HOME/.vimrc https://raw.githubusercontent.com/kartik1998/dotfiles/master/vim/.vimrc
 
 # install plugins
 vi +PlugInstall +qall
